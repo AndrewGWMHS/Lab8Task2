e3da210c9d5f4d65db85a35b67a9df1dabd40c5a
AndrewGWMHS <goff.614615@swl.k12.oh.us>
2024-11-04 08:26:17 -0500

    3

--- src/FavNumbers.java
+++ src/FavNumbers.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class FavNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num1 = InputHelper.getInt(scan, "Enter your favorite integer:");
+        double num2 = InputHelper.getDouble(scan, "Enter your favorite double:");
+        System.out.println("Your favorite integer is " + num1 + " and your favorite double is " + num2 + ".");
+    }
 }
